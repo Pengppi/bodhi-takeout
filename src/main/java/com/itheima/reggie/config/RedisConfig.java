316c96672429f20f059ebecfad9954ac53bd0d0c
西山对寂寥 <1781287641@qq.com>
2023-03-20 15:34:52 +0800

    redis环境配置

---
+++ src/main/java/com/itheima/reggie/config/RedisConfig.java
@@ -0,0 +1,29 @@
+/**
+ * @Author: Neo
+ * @Date: 2023/03/20 星期一 15:29:39 下午
+ * @Project: reggie
+ * @IDE: IntelliJ IDEA
+ **/
+
+package com.itheima.reggie.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        //默认的序列化方式是JdkSerializationRedisSerializer
+        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+        //redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
